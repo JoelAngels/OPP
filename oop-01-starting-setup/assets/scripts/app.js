f14cc9615cee394507e4e149875719634f0701f2
Joel Angel <joelbaraka4325@gmail.com>
2021-11-03 18:32:16 +0300

    changed it to classes

--- oop-01-starting-setup/assets/scripts/app.js
+++ oop-01-starting-setup/assets/scripts/app.js
@@ -1,19 +1,39 @@
+class Product {
+  title = "DEFAULT";
+  imageUrl;
+  description;
+  price;
+
+  constructor(title, image, description, price) {
+    this.title = title;
+    this.imageUrl = image;
+    this.description = description;
+    this.price = price;
+  }
+}
+
 const productList = {
   products: [
-    {
-      title: "Apex legends Merchandise",
-      imageUrl:
-        "//cdn.shopify.com/s/files/1/0256/5505/0326/products/AL200112-1.jpg?v=1620743476",
-      price: "$33:00",
-      description: "APEX LEGENDS KNOCKDOWN HOODED PULLOVER",
-    },
-    {
-      title: "Xbox Series X",
-      imageUrl:
-        "https://image.shutterstock.com/image-photo/ljubljana-slovenia-2020-march-18-600w-1677370546.jpg",
-      price: "$500:00",
-      description: "Microsoft Xbox Series X",
-    },
+    new Product(
+      "PS5",
+      "https://cdn.vox-cdn.com/thumbor/nEmwnn3PJyXqoKcr37D3T1o8AiQ=/1400x1050/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/21991383/vpavic_4261_20201023_0058.jpg",
+      "PS5 ON 50% OFF SALE !!!0",
+      `$500`
+    ),
+    new Product(
+      "Apex legends Merchandise",
+      "//cdn.shopify.com/s/files/1/0256/5505/0326/products/AL200112-1.jpg?v=1620743476",
+      "PS5 ON 50% OFF SALE !!!0",
+      `$33`,
+      "APEX LEGENDS KNOCKDOWN HOODED PULLOVER"
+    ),
+    new Product(
+      "Xbox Series X",
+      "https://image.shutterstock.com/image-photo/ljubljana-slovenia-2020-march-18-600w-1677370546.jpg",
+      "PS5 ON 50% OFF SALE !!!0",
+      `$500`,
+      "Microsoft Xbox Series X"
+    ),
   ],
   render() {
     const renderHook = document.getElementById("app");
