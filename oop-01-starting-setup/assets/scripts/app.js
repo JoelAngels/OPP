2c7f92767c80eee5741a6d58db91548526a714c5
Joel Angel <joelbaraka4325@gmail.com>
2021-11-04 12:20:33 +0300

    rendering a single product element

--- oop-01-starting-setup/assets/scripts/app.js
+++ oop-01-starting-setup/assets/scripts/app.js
@@ -1,9 +1,4 @@
 class Product {
-  title = "DEFAULT";
-  imageUrl;
-  description;
-  price;
-
   constructor(title, image, description, price) {
     this.title = title;
     this.imageUrl = image;
@@ -12,8 +7,31 @@ class Product {
   }
 }
 
-const productList = {
-  products: [
+//Renders are single product Element
+class ProductItem {
+  constructor(product) {
+    this.product = product;
+  }
+
+  render() {
+    const prodEl = document.createElement("li");
+    prodEl.className = "product-item";
+    prodEl.innerHTML = `
+      <div>
+        <img src = "${this.product.imageUrl}" alt = "${this.product.title}"/>
+        <div class = "product-item__content">
+            <h2>${this.product.title}</h2>
+            <h3>\$${this.product.price}</h3>
+            <p>${this.product.description}</p>
+            <button>Add to Cart</button>
+        </div>
+      </div>
+      `;
+    return prodEl;
+  }
+}
+class ProductList {
+  products = [
     new Product(
       "PS5",
       "https://cdn.vox-cdn.com/thumbor/nEmwnn3PJyXqoKcr37D3T1o8AiQ=/1400x1050/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/21991383/vpavic_4261_20201023_0058.jpg",
@@ -34,29 +52,22 @@ const productList = {
       `$500`,
       "Microsoft Xbox Series X"
     ),
-  ],
+  ];
+
+  constructor() {}
+
   render() {
     const renderHook = document.getElementById("app");
     const prodList = document.createElement("ul");
     prodList.className = "product-list";
     for (const prod of this.products) {
-      const prodEl = document.createElement("li");
-      prodEl.className = "product-item";
-      prodEl.innerHTML = `
-      <div>
-        <img src = "${prod.imageUrl}" alt = "${prod.title}"/>
-        <div class = "product-item__content">
-            <h2> ${prod.title}</h2>
-            <h2> ${prod.price}</h2>
-            <p> ${prod.description}</p>
-            <button>Add to Cart</button>
-        </div>
-      </div>
-      `;
+      const productItem = new ProductItem(prod);
+      const prodEl = productItem.render();
       prodList.append(prodEl);
     }
     renderHook.append(prodList);
-  },
-};
+  }
+}
 
+const productList = new ProductList();
 productList.render();
